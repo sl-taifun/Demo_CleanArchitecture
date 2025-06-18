b62ea4f47641349a14447897290987b566ffcec3
steph <ledouppe.s@gmail.com>
2025-06-18 17:10:50 +0200

    ajout des tests

---
+++ DemoCleanArchitecture.ApplicationCore.Test/Fakes/FakeGetAuthorRepository.cs
@@ -0,0 +1,65 @@
+﻿using DemoCleanArchitecture.ApplicationCore.Interfaces.Repositories;
+using DemoCleanArchitecture.Domain.Modeles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCleanArchitecture.ApplicationCore.Test.Fakes
+{
+    internal class FakeGetAuthorRepository : IAuthorRepository
+    {
+        private static List<Author> _authors = new List<Author>
+        {
+            new Author
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Pseudo = null,
+                BirthDate = new DateTime(1990, 1, 1)
+            },
+            new Author
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Smith",
+                Pseudo = "janesmith",
+                BirthDate = new DateTime(1985, 5, 15)
+            },
+            new Author
+            {
+                Id = 2,
+                FirstName = "Stephan",
+                LastName = "Ledouppe",
+                Pseudo = "Froggy",
+                BirthDate = new DateTime(1985, 5, 15)
+            }
+        };
+        public Author Create(Author data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAll(int offset, int limit)
+        {
+            return _authors.Skip(offset).Take(limit);
+        }
+
+        public Author? GetById(long id)
+        {
+            return _authors.FirstOrDefault(a => a.Id == id);
+        }
+
+        public Author Update(long id, Author data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DemoCleanArchitecture.ApplicationCore.Test/Services/AuthorServiceTest.cs
@@ -0,0 +1,93 @@
+﻿using DemoCleanArchitecture.ApplicationCore.Interfaces.Repositories;
+using DemoCleanArchitecture.ApplicationCore.Services;
+using DemoCleanArchitecture.ApplicationCore.Test.Fakes;
+using DemoCleanArchitecture.Domain.Modeles;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCleanArchitecture.ApplicationCore.Test.Services
+{
+    public class AuthorServiceTest
+    {
+        [Fact]
+        public void Get_AllAuthors_ShouldReturnAuthorsResult()
+        {
+
+            // Arrange
+            List<Author> expectedResult = new List<Author>
+            {
+            new Author
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Pseudo = null,
+                BirthDate = new DateTime(1990, 1, 1)
+            },
+            new Author
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Smith",
+                Pseudo = "janesmith",
+                BirthDate = new DateTime(1985, 5, 15)
+            },
+            new Author
+            {
+                Id = 2,
+                FirstName = "Stephan",
+                LastName = "Ledouppe",
+                Pseudo = "Froggy",
+                BirthDate = new DateTime(1985, 5, 15)
+            }
+        };
+            IAuthorRepository authorRepository = new FakeGetAuthorRepository();
+            var authorService = new AuthorService(authorRepository);
+            // Act
+            var authors = authorService.GetAll(0, 10);
+            // Assert
+            Assert.NotNull(authors);
+            Assert.Equal(3, authors.Count());
+            Assert.Equivalent(expectedResult, authors);
+        }
+    
+
+    public void Get_AuthorById_ShouldReturnOneAuthorResult()
+        {
+            var mockAuthor = new Author
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Pseudo = null,
+                BirthDate = new DateTime(1990, 1, 1)
+            };
+
+            var expectedResult = new Author
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Pseudo = null,
+                BirthDate = new DateTime(1990, 1, 1)
+            };
+            Mock<IAuthorRepository> mockAuthorRepository = new Mock<IAuthorRepository>();
+            mockAuthorRepository.Setup(repo => repo.GetById(2))
+                .Returns(mockAuthor);
+
+            AuthorService authorService = new AuthorService(mockAuthorRepository.Object);
+
+            var actualResult = authorService.GetById(2);
+
+            Assert.Equivalent(expectedResult, actualResult);
+            mockAuthorRepository.Verify(repo => repo.GetById(2), Times.Once);
+
+
+
+        }
+    }
+}
